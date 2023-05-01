b897f6d1c09b48938c377463ddf1b8845ac786c2
서범규 <101635957+alexfirst09@users.noreply.github.com>
2023-05-01 18:00:22 +0900

    클래스 3개 추가(임시)
    일단 클래스 3개 추가함 추후 더 추가 예정

--- plan.cpp
+++ plan.cpp
@@ -1,1 +1,55 @@
-// �ʿ��� ��ü�� ����� �����غ���(� Ŭ����, � ����, � �Լ� ?)
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+using namespace sf;
+
+// 필요한 객체와 기능을 생각해보기(어떤 클래스, 어떤 변수, 어떤 함수 ?)
+//발사할 고양이 클래스
+class Jumping_Cat
+{
+private:
+  //변수(필드)
+  //위치좌표, 크기, 각도, 파워(속도?, 가속도?), 공격력(파워와 같을 수 있음, 장애물을 파괴하기 위함)
+  
+public:
+  //함수(메소드)
+  //생성자, 소멸자?
+  //위치좌표, 크기, 각도, 파워(속도?, 가속도?), 공격력 setter/getter
+  //이동(move)
+  //크기조절(마우스로 당기면 늘어나도록 떼면 줄어들도록)(크기 setter/getter랑 같을 수도)
+  //각도조절(마우스 위치에 따라서 변경)(각도 setter/getter랑 같을 수도)
+};
+
+//통조림 클래스
+class Canned_Food
+{
+private:
+  //변수(필드)
+  //위치좌표, 크기
+  
+public:
+  //함수(메소드)
+  //생성자, 소멸자?
+  //위치좌표, 크기 setter/getter
+};
+
+//장애물1 클래스
+class Obstacle1
+{
+private:
+  //변수(필드)
+  //위치좌표, 크기, 장애물과 충돌했을때 장애물의 움직임(각도, 속도/가속도(중력가속도?), 등)->장애물이 움직이지 않고 그냥 장애물이 바로 파괴되는 방식으로 할지 논의 필요
+  //+강도(장애물의 체력)
+  
+public:
+  //함수(메소드)
+  //생성자, 소멸자?
+  //위치좌표, 크기, 강도, 등 setter/getter
+  //이동(move)?
+  //데미지 판정(고양이의 공격력으로 장애물의 체력 감소 적용)
+};
