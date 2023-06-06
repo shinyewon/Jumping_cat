3b3433023b50cd683a5cfdeae708adb0925c5b00
pjs <qaswzx321@gmail.com>
2023-06-06 21:22:08 +0900

    test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jumping_cat
+협업 툴: Visual Studio 2022
+
+고양이 클립 아트 PNG는 588ku에 의해 설계되었고,에서 유래되었다. https://kor.pngtree.com/freepng/a-running-three-eyed-cat_4488733.html?sol=downref&id=bef
+
+사람
+<a href='https://kor.pngtree.com/freepng/cartoon-long-hair-girl-silhouette_4283420.html'>만화 png에서 .pngtree.com/</a>
+
+통조림
+<a href="https://www.flaticon.com/kr/free-icon/canned-food_405662?term=%ED%86%B5%EC%A1%B0%EB%A6%BC&page=1&position=5&origin=search&related_id=405662">참치 아이콘  제작자: Freepik - Flaticon</a>
+
+조명
+<a href='https://kor.pngtree.com/freepng/floodlight_4842088.html'>투광 png에서 .pngtree.com/</a>
---
+++ explain.cpp
@@ -0,0 +1,58 @@
+﻿//Arc class 설명 부탁드립니다!
+//Arc class의 이동 메소드
+/*
+void moveArc(float dt)
+	{
+		setArcPos(getArcPosX() + getArcVelocityX() * dt, getArcPosY() - getArcVelocityY() * dt);
+
+		setArcVelocityX(getArcVelocityX() + 0 * dt);
+		setArcVelocityY(getArcVelocityY() - getArcAcceleration() * dt);
+	}
+*/
+
+//fortress_ex.cpp 파일의 포물선(arc)조절 및 포탄(고양이) 발사(이동) 방법
+/*
+{
+if (isAim == true)
+		{
+			int tLDrawCount = 0;
+
+			tl.setTLPosX(tl.getCurrTLPositionX());
+			tl.setTLPosY(tl.getCurrTLPositionY());
+
+			tl.setTLVelocityX(a.getAPower());
+			tl.setTLVelocityY(a.getAAngle());
+
+			for (float tldt = 0; tldt <= 0.02; tldt += 0.0001)
+			{
+				if (tLDrawCount == 0 || tLDrawCount % 10 == 0)
+				{
+					window.draw(tl.getTargetLine());
+				}
+
+				tl.moveTargetLine(tldt);
+				tLDrawCount++;
+			}
+		}
+
+
+		if (dt <= 0.5 && isFire == true)
+		{
+			isMoving = true;
+
+			window.draw(c.getCannonBall());
+
+			c.moveCannonBall(0.02);
+		}
+		else
+		{
+			isFire = false;
+			isMoving = false;
+		}
+
+		dt += 0.001;
+
+		// 화면을 표시한다. 
+		window.display();
+	}
+*/
